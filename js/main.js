c4ee6d911d4b5f64499bb704636d6bfa09680a6a
Greg Rowe <growe22@ufl.edu>
2016-11-30 17:15:50 -0600

    add slimMenu responsive navigation

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//responsive menu - slimmenu
+$('#navigation').slimmenu(
+    {
+        resizeWidth: '800',
+        collapserTitle: '',
+        animSpeed: 'medium',
+        easingEffect: null,
+        indentChildren: false,
+        childrenIndenter: '&nbsp;'
+    });
