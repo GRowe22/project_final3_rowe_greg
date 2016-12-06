d6981cb6ed6c129541271c9bbeff616de303e191
Greg Rowe <growe22@ufl.edu>
2016-12-05 20:28:44 -0600

    add custom JavaScript to add content

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ $('#navigation').slimmenu(
 $(document).ready(function () {
     $('#sticker').sticky({topSpacing: 0});
 });
+
+//Custom jQuery to change h1 colors when clicked
+$('h1').click(function () {
+    $('h1').css('color', '#ee582b');
+});
+
+//Custom JavaScript to add content when h1 clicked
+var heading = document.getElementById('click-h1');
+
+function addElement() {
+    var paragraph = document.createElement('p');
+    var node = document.createTextNode('We are awesome!');
+    paragraph.appendChild(node);
+    var element = document.getElementById('click-h1');
+    element.appendChild(paragraph);
+}
+
+heading.addEventListener('click', addElement);
